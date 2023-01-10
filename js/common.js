bec2ee5c40810ccf893c7430996be03dde130735
Kirill Nikolaevskiy <happykust@list.ru>
2023-01-10 18:50:34 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -10,7 +10,7 @@ const platform = window.innerWidth <= 1200 ? "mobile" : "pc";
 
 if (window.navigator.userAgent.includes("iPhone")) {
     $('#apple-fix').append(
-        ".swimming-photo_item {width: 30%!important}"
+        ".swimming-photo_item {width: 28%!important}"
     )
 }
 
